d7eb59b82d7fb6c24418d4ca51e5cc5fae1f00ff
Alvaro Flores <alvaro.flores01@gmail.com>
2017-02-08 13:19:08 -0800

    initial commit

---
+++ car.php
@@ -0,0 +1,23 @@
+<?php
+    class Car
+    {
+      public $model;
+      public $make;
+      public $price;
+      public $miles;
+
+    }
+
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8">
+     <title>Cars</title>
+   </head>
+   <body>
+
+   </body>
+ </html>
